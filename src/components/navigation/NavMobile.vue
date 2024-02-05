df4b7ca2303d35217dca335b3de0cb46aff0868b
Thijs van Brink <tvbr@axi.nl>
2024-02-05 14:16:04 +0100

    commit

--- src/components/navigation/NavMobile.vue
+++ src/components/navigation/NavMobile.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-// import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
-import SettingsMenu from "./SettingsMenu.vue";
 
 // eslint-disable-next-line no-unused-vars
 const emit = defineEmits<{
@@ -17,7 +15,7 @@ const openSidebarHandler = () => {
 <template>
   <div id="navbar" class="mobile">
     <div
-      class="flex justify-content-between flex-wrap bg-primary text-white fixed w-full"
+      class="flex justify-content-between flex-wrap bg-primary text-white fixed w-full px-2"
     >
       <div class="flex align-items-center justify-content-center">
         <Button
@@ -32,12 +30,21 @@ const openSidebarHandler = () => {
         />
       </div>
       <div class="brand flex align-items-center justify-content-center">
-        <router-link class="no-underline font-semibold text-color-white" to="/"
+        <router-link class="no-underline font-semibold text-white" to="/"
           >vanbr.ink</router-link
         >
       </div>
       <div class="flex align-items-center justify-content-center">
-        <SettingsMenu />
+        <Button
+          @click="openSidebarHandler"
+          icon="pi pi-bars"
+          severity="contrast"
+          text
+          rounded
+          aria-label="Bookmark"
+          style="width: 2rem; height: 2rem"
+          class="text-white"
+        />
       </div>
     </div>
   </div>
