cf36742be5a03d20f6327acace751fa583fda0b0
Thijs van Brink <tvbr@axi.nl>
2024-02-16 20:25:41 +0100

    commit

--- src/pages/DashboardView.vue
+++ src/pages/DashboardView.vue
@@ -5,7 +5,15 @@ const { user } = useAuth0();
 </script>
 
 <template>
-  <div class="flex justify-content-center flex-wrap pt-5">
+  <!-- <div class="flex justify-content-center flex-wrap pt-5">
     <div style="max-width: 500px">{{ user }}</div>
+  </div> -->
+  <!-- <div class="grid">
+    <div class="col-6 col-offset-3">
+      {{ user }}
+    </div>
+  </div> -->
+  <div class="pt-5 px-3">
+    {{ user }}
   </div>
 </template>
